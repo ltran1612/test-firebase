fb72836c5a0cb2de91fbc528b686f265c8fe9bba
Long Tran <longdtranw@gmail.com>
2024-06-02 02:39:19 -0600

    a

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -7,26 +7,23 @@ import {
 import { auth } from "@/src/lib/firebase/clientApp";
 
 export function onAuthStateChanged(cb) {
-  return () => {};
+  return _onAuthStateChanged(auth, cb);
 }
 
 export async function signInWithGoogle() {
-  
-  const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-  try {
-    await signInWithPopup(auth, provider);
-  } catch (error) {
-    console.error("Error siging in with Google", error)
-  }
-  return;
+try {
+await signInWithPopup(auth, provider);
+} catch (error) {
+console.error("Error signing in with Google", error);
+}
 }
 
 export async function signOut() {
-  try {
-    return auth.signOut();
-  } catch (error) {
-    console.error("Error signing out with Google", error);
-  }
-  return;
+try {
+return auth.signOut();
+} catch (error) {
+console.error("Error signing out with Google", error);
+}
 }
