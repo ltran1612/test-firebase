4427b057410d7012ac310a7d45345634aeeb12b6
Long Tran <longdtranw@gmail.com>
2024-06-02 11:16:49 -0600

    update

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -24,6 +24,7 @@ self.addEventListener("fetch", (event) => {
 });
 
 async function fetchWithFirebaseHeaders(request) {
+  console.log(firebaseConfig);
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const installations = getInstallations(app);
